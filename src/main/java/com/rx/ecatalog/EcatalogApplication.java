0c06f5c2f282ee13cf5a086738d76ab3d0300983
Rille <rille21@gmail.com>
2024-04-05 10:21:10 -0300

    Initial project

---
+++ src/main/java/com/rx/ecatalog/EcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.rx.ecatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rx/ecatalog/EcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rx.ecatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
