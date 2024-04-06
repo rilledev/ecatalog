e739a777629b837971609e8507a45e08d6bd3eb5
Rille <rille21@gmail.com>
2024-04-06 14:48:18 -0300

    Search by id

--- backend/src/main/java/com/rx/ecatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/rx/ecatalog/resources/CategoryResource.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rx.ecatalog.model.Category;
+import com.rx.ecatalog.dto.CategoryDTO;
 import com.rx.ecatalog.service.CategoryService;
 
 @RestController
@@ -19,8 +20,15 @@ public class CategoryResource {
 	private CategoryService service;
 
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+	
+	@RequestMapping(value = "/{id}")
+	@GetMapping
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+		CategoryDTO dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
 	}
 }
--- backend/src/main/java/com/rx/ecatalog/service/CategoryService.java
+++ backend/src/main/java/com/rx/ecatalog/service/CategoryService.java
@@ -1,11 +1,14 @@
 package com.rx.ecatalog.service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.rx.ecatalog.dto.CategoryDTO;
 import com.rx.ecatalog.model.Category;
 import com.rx.ecatalog.repositories.CategoryRepository;
 
@@ -18,7 +21,15 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.get();
+		return new CategoryDTO(entity);
 	}
 }
