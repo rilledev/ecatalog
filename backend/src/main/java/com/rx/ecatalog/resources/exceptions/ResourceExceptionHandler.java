9d2d8c89a703c8bd2d78d79d6c56a3eddb0ef07a
Rille <rille21@gmail.com>
2024-04-08 11:28:20 -0300

    Update category

--- backend/src/main/java/com/rx/ecatalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/rx/ecatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -7,15 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.rx.ecatalog.service.exceptions.EntityNotFoundException;
+import com.rx.ecatalog.service.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandarError> entityNotFound(EntityNotFoundException e, HttpServletRequest request){
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandarError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request){
 		StandarError err = new StandarError();
 		err.setTimestamp(Instant.now());
 		err.setStatus(HttpStatus.NOT_FOUND.value());
